9e555ba6a51071f41f6a552fb55e819824edd3ef
Khalil <jemghili200a@gmail.com>
2021-08-08 19:02:26 +0100

    Mise a jour

--- pac/urls.py
+++ pac/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', detail, name='home'),
-    path('/produit/', pro, name='produit'),
+    path('produit/', pro, name='produit'),
 ]
 
 
