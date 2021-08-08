9118c1784fc263c6cf4d9b463ca09698b04f43cd
Khalil <jemghili200a@gmail.com>
2021-08-08 14:04:50 +0100

    Mise a jour

--- common/views.py
+++ common/views.py
@@ -1,12 +1,3 @@
-from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView
-from .forms import SignUpForm, UserForm, ProfileForm
-from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect
-from django.contrib import messages
-from userprofile.models import Profile
 from .models import *
 from django.views import generic
 
