d35edd3635b24d50b81fa36a8a2f227b82637e87
Khalil <jemghili200a@gmail.com>
2021-08-11 20:47:10 +0100

    Mise a jour

--- pac/urls.py
+++ pac/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
-
+from django.conf import settings
+from django.conf.urls.static import static
 from common.views import *
 
 from django.contrib.auth import views as auth_views
@@ -12,10 +12,6 @@ urlpatterns = [
     path('', detail, name='home'),
     path('produit/', pro, name='produit'),
 ]
-
-
-from django.conf import settings
-from django.conf.urls.static import static
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
