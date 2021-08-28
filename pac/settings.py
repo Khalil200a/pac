5b502078a7cb209242310b367271ee363f833558
Khalil <jemghili200a@gmail.com>
2021-08-28 22:46:41 +0100

    Mise a jour

--- pac/settings.py
+++ pac/settings.py
@@ -135,9 +135,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
-)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media Files
